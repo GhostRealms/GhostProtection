61a8853992cde4277419f83498ae4eb8eb5fc703
River Marmorstein <river@rmarmorstein.com>
2015-01-27 23:40:13 -0500

    moved some stuff around to fix the project structure

---
+++ src/main/java/net/ghostrealms/protection/GhostProtection.java
@@ -0,0 +1,23 @@
+package net.ghostrealms.protection;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by River on 1/27/2015.
+ * Ghost Realms Chunk based Protection Suite
+ * This Plugin is intended to be used as an API for the Metropolis Suite, on it's own it has limited functionality. 
+ */
+
+public class GhostProtection extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+
+    }
+    
+}
